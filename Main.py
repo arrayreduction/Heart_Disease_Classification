c4fd31ac9938b8666f4276b106aa822011e7bdd7
arrayreduction <yblad@hotmail.co.uk>
2022-04-11 15:18:09 +0100

    More EDA

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import pre_process
 
 def main():
-    pre_process.main()
+    df = pre_process.main(run_eda=True)
 
 if __name__ == '__main__':
     main()
--- pre_process.py
+++ pre_process.py
@@ -3,15 +3,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def eda(df):
+    '''Exploratory Data Analysis (EDA)'''
     print(df.head())
     print(df.info())
 
+    for i, column in enumerate(df.columns, 1):
+        plt.subplot(3,3,i)
+        sns.histplot(df[column])
+
+    #df.plot.hist(subplots=True)
+    #plt.show()
+
+    #create pairplot
     sns.pairplot(df)
     plt.show()
 
-    return
-
-def main():
-    eda(df=pd.read_csv("heart.csv"))
+    #pairplot excluding 
 
     return
+
+def main(run_eda=False):
+    df = pd.read_csv("heart.csv")
+
+    if run_eda:
+        eda(df)
+
+    return df
